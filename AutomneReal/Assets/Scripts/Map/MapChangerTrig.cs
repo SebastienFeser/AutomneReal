6d50ee32683f5f77836683490514afaffef6e483
Sebastien Feser <sedukafe@gmail.com>
2019-01-10 10:59:58 +0100

    First push

---
+++ AutomneReal/Assets/Scripts/Map/MapChangerTrig.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapChangerTrig : MonoBehaviour
+{
+    [SerializeField] GameObject map;
+    [SerializeField] GameObject player;
+    float TileSizeX = 18f;
+    float TileSizeY = 10f;
+    [SerializeField] float waitForSeconds;
+    float playerMoveX = 0.165f;
+    float playerMoveY = 0.155f;
+
+    Vector2 startPosition;
+    Vector2 endPosition;
+    enum TriggerPosition
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT,
+    }
+
+    [SerializeField] TriggerPosition triggerPosition;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "PlayerCheck")
+        {
+            if (!player.GetComponent<PlayerMovements>().mapMoving)
+            {
+                switch (triggerPosition)
+                {
+                    case TriggerPosition.UP:
+                        MoveUp();
+                        break;
+                    case TriggerPosition.DOWN:
+                        MoveDown();
+                        break;
+                    case TriggerPosition.LEFT:
+                        MoveLeft();
+                        break;
+                    case TriggerPosition.RIGHT:
+                        MoveRight();
+                        break;
+                }
+            }
+        }
+    }
+
+    void MoveUp()
+    {
+        StartCoroutine("MoveUpCor");
+    }
+
+    void MoveDown()
+    {
+        StartCoroutine("MoveDownCor");
+    }
+
+    void MoveLeft()
+    {
+        StartCoroutine("MoveLeftCor");
+    }
+
+    void MoveRight()
+    {
+        StartCoroutine("MoveRightCor");
+    }
+  
+    IEnumerator MoveUpCor()
+    {
+        player.GetComponent<PlayerMovements>().mapMoving = true;
+        startPosition = map.transform.position;
+        endPosition = new Vector2(map.transform.position.x, map.transform.position.y - TileSizeY);
+        for (float i = 0; i < TileSizeY; i += 0.18f)
+        {
+            player.transform.position = new Vector2(player.transform.position.x, player.transform.position.y - playerMoveY);
+            map.transform.position = new Vector2(startPosition.x, startPosition.y - i);
+            yield return new WaitForSeconds(waitForSeconds);
+        }
+        player.GetComponent<PlayerMovements>().mapMoving = false;
+        map.transform.position = endPosition;
+    }
+
+    IEnumerator MoveDownCor()
+    {
+        player.GetComponent<PlayerMovements>().mapMoving = true;
+        startPosition = map.transform.position;
+        endPosition = new Vector2(map.transform.position.x, map.transform.position.y + TileSizeY);
+        for (float i = 0; i < TileSizeY; i += 0.18f)
+        {
+            player.transform.position = new Vector2(player.transform.position.x, player.transform.position.y + playerMoveY);
+            map.transform.position = new Vector2(startPosition.x, startPosition.y + i);
+            yield return new WaitForSeconds(waitForSeconds);
+        }
+        player.GetComponent<PlayerMovements>().mapMoving = false;
+        map.transform.position = endPosition;
+    }
+
+    IEnumerator MoveLeftCor()
+    {
+        player.GetComponent<PlayerMovements>().mapMoving = true;
+        startPosition = map.transform.position;
+        endPosition = new Vector2(map.transform.position.x + TileSizeX, map.transform.position.y);
+        for (float i = 0; i < TileSizeX; i += 0.18f)
+        {
+            player.transform.position = new Vector2(player.transform.position.x + playerMoveX, player.transform.position.y);
+            map.transform.position = new Vector2(startPosition.x + i, startPosition.y);
+            yield return new WaitForSeconds(waitForSeconds);
+        }
+        player.GetComponent<PlayerMovements>().mapMoving = false;
+        map.transform.position = endPosition;
+    }
+
+    IEnumerator MoveRightCor()
+    {
+        player.GetComponent<PlayerMovements>().mapMoving = true;
+        startPosition = map.transform.position;
+        endPosition = new Vector2(map.transform.position.x - TileSizeX, map.transform.position.y);
+        for (float i = 0; i < TileSizeX; i += 0.18f)
+        {
+            player.transform.position = new Vector2(player.transform.position.x - playerMoveX, player.transform.position.y);
+            map.transform.position = new Vector2(startPosition.x - i, startPosition.y);
+            yield return new WaitForSeconds(waitForSeconds);
+        }
+        player.GetComponent<PlayerMovements>().mapMoving = false;
+        map.transform.position = endPosition;
+    }
+}
---
+++ AutomneReal/Assets/Scripts/Player/PlayerMovements.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] Rigidbody2D playerRigidbody2D;
+    public bool mapMoving = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(mapMoving);
+        if (!mapMoving)
+        {
+            playerRigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movementSpeed, Input.GetAxisRaw("Vertical") * movementSpeed);
+        }
+        else
+        {
+            playerRigidbody2D.velocity = new Vector2(0, 0);
+        }
+
+    }
+}
