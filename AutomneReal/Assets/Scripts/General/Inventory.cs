5c2a09de505bfabca018d783d6f2a7d7da424cbb
Sebastien Feser <sedukafe@gmail.com>
2019-01-13 16:04:53 +0100

    Stage created

---
+++ AutomneReal/Assets/Scripts/General/Inventory.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    int wood;               //Increase AxeSpeed
+    int mushrooms;          //Increase Speed
+    int enemyTears;         //Increase Damage
+    int stone;              //Increase Resistence
+}
